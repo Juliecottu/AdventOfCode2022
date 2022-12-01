51f565bc7b73d9948fed308185f960d491924aae
Juliecottu <juliecottu@gmail.com>
2022-12-01 20:59:58 +0100

    Resolve puzzle Day 1 - Part 1

--- scripts/day1.js
+++ scripts/day1.js
@@ -5,9 +5,17 @@ let response = fetch(url);
 
 response
   .then(data => data.text())
-  .then(function(string) {
-    let array = string.split('\n\n');
-    console.log(array.length);
-    // for(let elf )
+  .then(string => string.split('\n\n'))
+  .then(elves => {
+    console.log(elves);
+    let highestAmount = 0 ;
+    for(let elf of elves ) {
+      const elfCalo = elf.split('\n');
+      
+      let caloSum = elfCalo.reduce((acc, current) => acc + Number(current), 0);
 
-  } );
+      caloSum > highestAmount ? highestAmount =  caloSum : null;
+     }
+
+     console.log(highestAmount);
+  })
