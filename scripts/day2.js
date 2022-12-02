b30669803ece5b6edbb5669eb6c6e18137c6b887
Juliecottu <juliecottu@gmail.com>
2022-12-02 20:26:20 +0100

    Resolve puzzle Day 2 - Part 1

--- scripts/day2.js
+++ scripts/day2.js
@@ -1,60 +1,80 @@
 console.log("--day 2--");
 
-let url = 'https://127.0.0.1:8001/docs/day2.txt';
+let url = 'https://127.0.0.1:8000/docs/day2.txt';
+let response = fetch(url);
 
-async function f() {
+// const values = [
+//   ['A', 1],
+//   ['B', 2],
+//   ['C', 3],
+//   ['X', 1],
+//   ['Y', 2],
+//   ['Z', 3],
+//   ['A > Me', 0],
+//   ['A = Me', 3],
+//   ['A < Me', 6],
 
-  try {
-    let response = await fetch(url);
-    let data = await response.text();
-    let array = data.split('\n');
-    return array;
-  } catch(err) {
-    // attrape les erreurs à la fois dans fetch et response.json
-    alert(err);
-  }
+// ];
+
+const values = {
+  'Rock': 1,
+  'Paper': 2,
+  'Scissors': 3,
+  'loses': 0,
+  'draw': 3,
+  'wins': 6,
 }
 
-let rawArray = f();
-console.dir(rawArray);
+response
+  .then(data => data.text())
+  .then(data => {
+    // console.log(data.replace(/ /g, 'Rock'));
+    let newString = data.replace(/A|X/g, 'Rock');
+    newString = newString.replace(/B|Y/g, 'Paper');
+    newString = newString.replace(/C|Z/g, 'Scissors');
+    return newString;
+  })
+  .then(data => data.split('\n'))
+  .then(data => {
+    let scores = [],
+      roundScore = 0,
+      shapeScore = 0;;
 
-// async function f() {
-//
-//   let promise = new Promise((resolve, reject) => {
-//     // let response = fetch(url);
-//
-//     fetch(url)
-//       .then(data => data.text())
-//       .then(data => data.split('\n'))
-//       .then(data => {
-//         let scores = [];
-//         for (let line of data) {
-//           console.log(line[0]);
-//           scores.push([line[0], line[2]]);
-//         }
-//       });
-//   });
-//
-//   let result = await promise; // attendre que la promesse soit résolue (*)
-//   console.log('eeee', result);
-//
-// }
-//
-// f();
+    for(let line of data) {
+
+      const shapes = line.split(' '),
+            playerA = values[String(shapes[0])], 
+            playerMe = values[String(shapes[1])];
+      let roundResult;
 
 
-// response
-//   .then(data => data.text())
-//   .then(data => data.split('\n'))
-  // .then(data => {
-  //   let playerA = [],
-  //     playerMe = [];
-  //   let scores = [];
-  //   for(let line of data) {
-  //     scores.push([line[0], line[2]]);
-  //   }
-  //   return scores;
-  // })
-// ;
+      if(Math.abs(playerA - playerMe) > 1) {
+        if(playerA > playerMe) {
+          roundResult = values['wins'];
+        } else {
+          roundResult = values['loses'];
+        }
+      } else if(Math.abs(playerA - playerMe) === 0) {
+        roundResult = values['draw'] ;
+      } else if(Math.abs(playerA - playerMe) === 1) {
+          if(playerA > playerMe) { 
+            roundResult = values['loses'];
+          } else {
+            roundResult = values['wins'];
+          }
+      }
 
-// console.log(response);
+      scores.push([playerA, playerMe, roundResult]);
+      roundScore += roundResult;
+      shapeScore += playerMe;
+
+      // console.group();
+      // console.log(shapes[0], shapes[1], roundResult);
+      // console.log(roundScore, shapeScore);
+      // console.groupEnd();
+    }
+
+    console.log(roundScore + shapeScore);
+    console.log(scores);
+  })
+;
