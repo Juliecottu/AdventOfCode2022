547d45ffbe7c6431e98570b78ceecdc730255b49
Juliecottu <juliecottu@gmail.com>
2022-12-02 21:35:55 +0100

    Resolve puzzle Day 2 - Part 2

--- scripts/day2.js
+++ scripts/day2.js
@@ -3,19 +3,6 @@ console.log("--day 2--");
 let url = 'https://127.0.0.1:8000/docs/day2.txt';
 let response = fetch(url);
 
-// const values = [
-//   ['A', 1],
-//   ['B', 2],
-//   ['C', 3],
-//   ['X', 1],
-//   ['Y', 2],
-//   ['Z', 3],
-//   ['A > Me', 0],
-//   ['A = Me', 3],
-//   ['A < Me', 6],
-
-// ];
-
 const values = {
   'Rock': 1,
   'Paper': 2,
@@ -23,6 +10,9 @@ const values = {
   'loses': 0,
   'draw': 3,
   'wins': 6,
+  'X':'loses',
+  'Y':'draw',
+  'Z':'wins',
 }
 
 response
@@ -32,7 +22,11 @@ response
     let newString = data.replace(/A|X/g, 'Rock');
     newString = newString.replace(/B|Y/g, 'Paper');
     newString = newString.replace(/C|Z/g, 'Scissors');
-    return newString;
+    let partTwoString = data.replace(/A/g, 'Rock');
+    partTwoString = partTwoString.replace(/B/g, 'Paper');
+    partTwoString = partTwoString.replace(/C/g, 'Scissors');
+    // return newString;
+    return partTwoString;
   })
   .then(data => data.split('\n'))
   .then(data => {
@@ -41,14 +35,15 @@ response
       shapeScore = 0;;
 
     for(let line of data) {
-
       const shapes = line.split(' '),
             playerA = values[String(shapes[0])], 
-            playerMe = values[String(shapes[1])];
+            playerMe = values[String(shapes[1])]; 
+      let expectedScore = values[String(shapes[1])];
       let roundResult;
 
 
-      if(Math.abs(playerA - playerMe) > 1) {
+      // Part 1
+/*       if(Math.abs(playerA - playerMe) > 1) {
         if(playerA > playerMe) {
           roundResult = values['wins'];
         } else {
@@ -62,19 +57,31 @@ response
           } else {
             roundResult = values['wins'];
           }
+      } */
+
+      // scores.push([playerA, playerMe, roundResult]);
+      // roundScore += roundResult;
+      //shapeScore += playerMe;
+
+
+      // Part 2
+      roundScore += values[expectedScore];
+      let playerMePartTwo;
+      if(expectedScore === 'loses') {
+        playerMePartTwo = playerA - 1;
+        playerMePartTwo <= 0 ? playerMePartTwo = 3 : null;
+      } else if (expectedScore === 'draw') {
+        playerMePartTwo = playerA ;
+      } else if(expectedScore === 'wins') {
+        playerMePartTwo = playerA + 1;
+        playerMePartTwo > 3 ? playerMePartTwo = 1 : null;
       }
 
-      scores.push([playerA, playerMe, roundResult]);
-      roundScore += roundResult;
-      shapeScore += playerMe;
+      shapeScore += playerMePartTwo;
 
-      // console.group();
-      // console.log(shapes[0], shapes[1], roundResult);
-      // console.log(roundScore, shapeScore);
-      // console.groupEnd();
+
     }
 
     console.log(roundScore + shapeScore);
-    console.log(scores);
   })
 ;
