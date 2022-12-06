994062f3b9be441f2bb533e9c81cc3a4426b5026
Julie <jcottu@umanit.fr>
2022-12-06 14:27:37 +0100

    Advent of code day 6 - Part 2

--- scripts/day6.js
+++ scripts/day6.js
@@ -7,8 +7,8 @@ async function findSubroutine() {
   const response = await fetch(url),
     data = await response.text();
 
-  const sub = checkThreePreviousChar(3);
-  console.log(sub, data.indexOf(sub) + 4);
+  const sub = checkThreePreviousChar(13);
+  console.log(sub, data.indexOf(sub) + 14);
 
   /**
    *
@@ -16,19 +16,25 @@ async function findSubroutine() {
    * @returns {string} the subRoutine String
    */
     function checkThreePreviousChar(i) {
-      for (let i = 3; i < data.length; i++) {
+      for (let i = 13; i < data.length; i++) {
         const studiedChar = data.charAt(i),
           previousChars = `${data.charAt(i - 3)}${data.charAt(i - 2)}${data.charAt(i - 1)}`;
 
+        let largePreviousChar = '';
+        for (let k = 13; k >= 1; k--) {
+          largePreviousChar += data.charAt(i - k);
+        }
+
         let setPreviousChars = new Set();
-        for(let j = 1; j < 4; j++) {
+        for(let j = 1; j < 14; j++) {
           setPreviousChars.add(data.charAt(i - j));
         }
-        if(setPreviousChars.size == 3) {
+        if(setPreviousChars.size == 13) {
           if(setPreviousChars.has(studiedChar)) {
             continue;
           } else {
-            return previousChars + studiedChar;
+            // return previousChars + studiedChar;
+            return largePreviousChar + studiedChar;
           }
         }
 
